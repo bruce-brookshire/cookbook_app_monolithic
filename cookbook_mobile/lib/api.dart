13c5b67f28dbb7d06c45db213603ceb5f64637a3
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-02-25 23:42:54 -0600

    added mobile app

---
+++ cookbook_mobile/lib/api.dart
@@ -0,0 +1,5 @@
+
+
+// class HTTPInterface {
+//   static const url = 
+// }
---
+++ cookbook_mobile/lib/new_entry_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewEntryView extends StatefulWidget {
+  @override
+  State createState() => _NewEntryViewState();
+}
+
+class _NewEntryViewState extends State<NewEntryView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
